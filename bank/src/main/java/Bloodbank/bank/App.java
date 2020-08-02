abaa0835a84c02b2f75821067823a80d8f83edbf
Elcot <Elcot@SATHISHKUMAR>
2020-08-02 23:38:58 +0530

    changs

---
+++ bank/src/main/java/Bloodbank/bank/App.java
@@ -0,0 +1,13 @@
+package Bloodbank.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
